c4c5a8e8580e10ebc18ee4c1fdbb9cf254b7c57a
João <joaombmrodrigues@gmail.com>
2024-03-21 23:28:00 +0000

    O programa recebe um numero de dados e uma seed e mostra a soma dos valores obtidos

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -3,10 +3,27 @@
 namespace RanDice
 {
     class Program
-    {
+    {   
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int n = int.Parse(args[0]); 
+            int s = int.Parse(args[1]); 
+
+            int sum = 0; 
+
+            Random rand = new Random(s);
+
+            for (int i = 0; i < n; i++)
+            {
+                
+                int dVal = rand.Next(1,7);
+                sum += dVal;
+                
+            }  
+            Console.WriteLine(sum);     
+            
         }
     }
 }
